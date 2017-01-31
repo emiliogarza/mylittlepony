1bdb46eae79412b91a62e2e8f3ab723bb59985ed
Emilio Garza <emilio@dev-e.com>
2017-01-30 19:22:12 -0800

    Just going to keep this simple by using an image only instead of SVG... for now

---
+++ script.js
@@ -0,0 +1,7 @@
+(function(unicorns) {
+	var unicorn = document.createElement('img');
+	unicorn.src = 'http://data.whicdn.com/images/104960945/large.png';
+	unicorn.alt = 'My Little Pony Unicorn!!!!';
+	unicorn.style = 'color:red;';
+	document.getElementsByTagName('body')[0].appendChild(unicorn);
+})();
